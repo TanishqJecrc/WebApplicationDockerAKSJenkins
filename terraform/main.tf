a08697509cf377d0280c1f2c0e7baec729cfaf8b
tanishqsingh.cse25 <tanishqsingh.cse25@jecrc.ac.in>
2025-04-15 16:06:32 +0530

    s

--- terraform/main.tf
+++ terraform/main.tf
@@ -39,15 +39,16 @@ resource "azurerm_kubernetes_cluster" "aks" {
 
 }
 
-resource "azurerm_role_assignment" "acr_pull" {
-    scope                = azurerm_container_registry.acr.id
-    role_definition_name = "AcrPull"
-    principal_id         = azurerm_kubernetes_cluster.aks.identity[0].principal_id
 
-    depends_on = [
+resource "azurerm_role_assignment" "acr_pull" {
+  scope                = azurerm_container_registry.acr.id
+  role_definition_name = "AcrPull"
+  principal_id         = azurerm_kubernetes_cluster.aks.identity[0].principal_id
+  depends_on = [
     azurerm_kubernetes_cluster.aks
   ]
-  }
+}
+
 
 # Outputs
 output "resource_group_name" {
